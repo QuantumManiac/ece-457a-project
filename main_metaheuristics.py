6df4b97fc9ede2bf650e94203c5005982a42735c
Cooper <cbrunke@uwaterloo.ca>
2023-11-21 15:28:30 -0500

    structure, pending local search

--- main_metaheuristics.py
+++ main_metaheuristics.py
@@ -13,6 +13,32 @@
 import random
 import typing
 import copy
+import subprocess
+import threading
+import time
+import sys
+
+
+class Global:
+    hyper_paramerters: typing.Dict = {'value': {'iter': 10, 'mutation_prob': 0.3},
+                                      'range': {'iter': [1, 100], 'mutation_prob': [0.0, 1.0]}}
+    snake_performance: typing.Dict = {'turns_alive': 0, 'num_kills': 0, 'snake_size': 1, 'avg_health': 100, 'won_game': False}
+
+    @classmethod
+    def reset_snake_performance(cls):
+        cls.snake_performance = {'turns_alive': 0, 'num_kills': 0, 'snake_size': 1, 'avg_health': 100, 'won_game': False}
+
+    @classmethod
+    def get_hyper_parameters(cls):
+        return cls.hyper_paramerters
+    
+    @classmethod
+    def set_hyper_parameters(cls, hyper_params: typing.Dict):
+        cls.hyper_paramerters = hyper_params
+    
+    @classmethod
+    def get_snake_performance(cls):
+        return cls.snake_performance
 
 
 # info is called when you create your Battlesnake on play.battlesnake.com
@@ -44,8 +70,11 @@ def end(game_state: typing.Dict):
 # Valid moves are "up", "down", "left", or "right"
 # See https://docs.battlesnake.com/api/example-move for available data
 def move(game_state: typing.Dict) -> typing.Dict:
-    
-    iter, mutation_prob = 10, 0.3
+
+    # iter, mutation_prob = 10, 0.3
+    hyper_params = Global.get_hyper_parameters()['value']
+    iter, mutation_prob = hyper_params["iter"], hyper_params["mutation_prob"]
+
     max_while = 25
     best_move_set, best_cost = None, -1
     safe_moves = [generate_moves(game_state,1),
@@ -214,11 +243,73 @@ def assess_cost(game_state: typing.Dict, proposed_moves: typing.List):
             body.pop()
         
         est_cost += 1
-        
+
     return est_cost
+
+def calculate_fitness(win_time_gain, survival_time_gain, kill_gain, avg_health_gain, won_game: bool):
+    a = 0 if not won_game else a
+    b = 0 if won_game else b
+    snake_performance = Global.get_snake_performance()
+
+    return (win_time_gain / snake_performance['turns_alive']) + \
+            survival_time_gain*snake_performance["turns_alive"] + \
+            kill_gain*snake_performance["snake_size"] + \
+            avg_health_gain*snake_performance['avg_health']
+
+def hyper_parameter_local_search(iter_per_set, total_iter):
+    # Cost function gain parameters
+    WIN_TIME_GAIN = 1
+    SURVIVAL_TIME_GAIN = 1
+    KILL_GAIN = 1
+    SIZE_GAIN = 1
+    AVG_HEALTH_GAIN = 1
+
+    hyper_parameters = Global.get_hyper_parameters()
+
+    # set initial hyperparams
+    
+
+    # get fitness for initial params
+    Global.set_hyper_parameters(hyper_parameters)
+
+    # run for a set number of iterations
+
+    calculate_fitness(WIN_TIME_GAIN, SURVIVAL_TIME_GAIN, KILL_GAIN, SIZE_GAIN, AVG_HEALTH_GAIN)
+
+
+    return hyper_parameters
+
+def run_game(run_in_browser: bool):
+    command = [
+        './battlesnake', 'play',
+        '--name', 'meta_snake',
+        '--url', 'http://127.0.0.1:8000',
+    ]
+
+    command += '--browser' if run_in_browser else command
+    subprocess.run(command)
 
 # Start server when `python main.py` is run
 if __name__ == "__main__":
     from server import run_server
+    HYPER_PARAMETER_OPTIMIZATION = False
+    # Add more parameters for more fitness function variables
 
-    run_server({"info": info, "start": start, "move": move, "end": end})
+
+    server_thread = threading.Thread(target=run_server, args=({"info": info, "start": start, "move": move, "end": end},))
+    server_thread.start()
+    time.sleep(0.1)
+
+    if HYPER_PARAMETER_OPTIMIZATION == False:
+        run_game(run_in_browser=True)
+        sys.exit()
+
+    INNER_LOOP_ITERATIONS = 10
+    OUTER_LOOP_ITERATIONS = 100
+
+    hyper_parameter_local_search(INNER_LOOP_ITERATIONS, OUTER_LOOP_ITERATIONS)
+
+    for _ in range(3):
+        run_game(False)
+        Global.hyper_paramerters['value']['mutation_prob'] += 0.3
+        Global.reset_snake_performance()
